721dd38222ab9a5ba46062262b3b20a88898cbf5
OmarAbdallaa <omar.abdallaa@gmail.com>
2018-11-27 10:48:43 +0100

    commit

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,10 +1,13 @@
 function type_check_v1(arg,type){
     if(type === "array"){
-        return ar1 instanceof Array;
+        return arg instanceof Array;
     }
     return typeof arg == type ;
 }
-function type_check_v2(arg,type){
+function type_check_v2(arg1,enumera){
+    if("type" in  enumera){
+        var is 
+    }
 
 
 }
