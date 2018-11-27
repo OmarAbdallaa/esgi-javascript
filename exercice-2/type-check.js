5a1aef129adc3efcbc91b15320ee248568d4f8ad
OmarAbdallaa <omar.abdallaa@gmail.com>
2018-11-27 10:56:08 +0100

    typev2
    Signed-off-by: OmarAbdallaa <omar.abdallaa@gmail.com>

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -6,7 +6,13 @@ function type_check_v1(arg,type){
 }
 function type_check_v2(arg1,enumera){
     if("type" in  enumera){
-        var is 
+        var isArray = enumera ['type'] === 'array';
+        if (isArray && !(arg1 instanceof Array)){
+            return false;
+
+        }else if (!isArray && typeof arg1 !== enumera['type']){
+            return false ;
+        }
     }
 
 
